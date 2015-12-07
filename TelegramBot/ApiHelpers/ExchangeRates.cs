e91b03679d86d2a794bf25995a38c5554eaccd08
Afterlife88 <extragalactic88@gmail.com>
2015-12-07 17:44:36 +0200

    added ignone

--- TelegramBot/ApiHelpers/ExchangeRates.cs
+++ TelegramBot/ApiHelpers/ExchangeRates.cs
@@ -8,7 +8,7 @@ namespace TelegramBot.ApiHelpers
         DateTime _dateTime = DateTime.UtcNow.Date;
         public GetRatesResponse GetRates()
         {
-            var api = new Oanda.ExchangeRates("KEY");
+            var api = new Oanda.ExchangeRates("iPWLHRIMsm0FntO9rtbor0E6");
             var responseGetRatesForSeveralQuotes = api.GetRates("USD",
                 quote: "EUR",
                 date: _dateTime.ToString("yyyy-dd-MM"),
